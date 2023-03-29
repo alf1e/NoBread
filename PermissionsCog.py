52c557601aca1b9734fa514b2e43c20307da4d2d
alf1e <alfiewbarlow@gmail.com>
2023-03-29 18:30:45 +0100

    Fix: Modules overriding cursour

--- PermissionsCog.py
+++ PermissionsCog.py
@@ -12,8 +12,15 @@ class PermissionsCog(ModuleCog):
     def __init__(self, module_id: str):
         super().__init__(module_id)
 
-        self.connection = sqlite3.connect(self.module.storage_path / "permissions.db")
-        self.cursor = self.connection.cursor()
+        self._connection = sqlite3.connect(self.module.storage_path / "permissions.db")
+        self._cursor = self._connection.cursor()
+        self._cursor.execute(
+            "CREATE TABLE IF NOT EXISTS permissions ("
+            "   permission_name TEXT PRIMARY KEY NOT NULL UNIQUE,"
+            "   users TEXT NOT NULL,"
+            "   roles TEXT NOT NULL"
+            ")"
+        )
         self.module_id = module_id
 
     def _check_module(self, permission_name: str, allow_all: bool) -> bool:
@@ -29,30 +36,31 @@ class PermissionsCog(ModuleCog):
     def create_permissions(self, permission_name: str) -> str:
         if self._check_module(permission_name, False):
             raise ValueError("Permission name must start with module id")
+        print(self.module_id)
 
-        if self.cursor.execute("SELECT permission_name FROM permissions WHERE permission_name = ?", (permission_name,)).fetchone():
+        if self._cursor.execute("SELECT permission_name FROM permissions WHERE permission_name = ?", (permission_name,)).fetchone():
             return permission_name
 
-        self.cursor.execute(
+        self._cursor.execute(
             "INSERT INTO permissions (permission_name, users, roles) VALUES (?, ?, ?)",
             (permission_name, "", "")
         )
-        self.connection.commit()
+        self._connection.commit()
         return permission_name
 
     def delete_permissions(self, permission_name: str) -> None:
         if self._check_module(permission_name, False):
             raise ValueError("Permission name must start with module id")
 
-        self.cursor.execute("DELETE FROM permissions WHERE permission_name = ?", (permission_name,))
-        self.connection.commit()
+        self._cursor.execute("DELETE FROM permissions WHERE permission_name = ?", (permission_name,))
+        self._connection.commit()
 
     def add_user(self, permission_name: str, user_id: int) -> None:
         user_id = str(user_id)
         if self._check_module(permission_name, False):
             raise ValueError("Permission name must start with module id")
 
-        users = self.cursor.execute("SELECT users FROM permissions WHERE permission_name = ?", (permission_name,)).fetchone()
+        users = self._cursor.execute("SELECT users FROM permissions WHERE permission_name = ?", (permission_name,)).fetchone()
         if users is None:
             users = []
         else:
@@ -61,30 +69,30 @@ class PermissionsCog(ModuleCog):
             return
         users.append(user_id)
         users = ",".join(users)
-        self.cursor.execute("UPDATE permissions SET users = ? WHERE permission_name = ?", (users, permission_name))
-        self.connection.commit()
+        self._cursor.execute("UPDATE permissions SET users = ? WHERE permission_name = ?", (users, permission_name))
+        self._connection.commit()
 
     def remove_user(self, permission_name: str, user_id: int) -> None:
         user_id = str(user_id)
         if self._check_module(permission_name, False):
             raise ValueError("Permission name must start with module id")
 
-        users = self.cursor.execute("SELECT users FROM permissions WHERE permission_name = ?", (permission_name,)).fetchone()
+        users = self._cursor.execute("SELECT users FROM permissions WHERE permission_name = ?", (permission_name,)).fetchone()
         if users is None:
             users = []
         else:
             users = users[0].split(",")
         users.remove(user_id)
         users = ",".join(users)
-        self.cursor.execute("UPDATE permissions SET users = ? WHERE permission_name = ?", (users, permission_name))
-        self.connection.commit()
+        self._cursor.execute("UPDATE permissions SET users = ? WHERE permission_name = ?", (users, permission_name))
+        self._connection.commit()
 
     def add_role(self, permission_name: str, role_id: int) -> None:
         role_id = str(role_id)
         if self._check_module(permission_name, False):
             raise ValueError("Permission name must start with module id")
 
-        roles = self.cursor.execute("SELECT roles FROM permissions WHERE permission_name = ?", (permission_name,)).fetchone()
+        roles = self._cursor.execute("SELECT roles FROM permissions WHERE permission_name = ?", (permission_name,)).fetchone()
         if roles is None:
             roles = []
         else:
@@ -93,29 +101,29 @@ class PermissionsCog(ModuleCog):
             return
         roles.append(role_id)
         roles = ",".join(roles)
-        self.cursor.execute("UPDATE permissions SET roles = ? WHERE permission_name = ?", (roles, permission_name))
-        self.connection.commit()
+        self._cursor.execute("UPDATE permissions SET roles = ? WHERE permission_name = ?", (roles, permission_name))
+        self._connection.commit()
 
     def remove_role(self, permission_name: str, role_id: int) -> None:
         user_id = str(role_id)
         if self._check_module(permission_name, False):
             raise ValueError("Permission name must start with module id")
 
-        roles = self.cursor.execute("SELECT roles FROM permissions WHERE permission_name = ?", (permission_name,)).fetchone()
+        roles = self._cursor.execute("SELECT roles FROM permissions WHERE permission_name = ?", (permission_name,)).fetchone()
         if roles is None:
             roles = []
         else:
             roles = roles[0].split(",")
         roles.remove(role_id)
         roles = ",".join(roles)
-        self.cursor.execute("UPDATE permissions SET roles = ? WHERE permission_name = ?", (roles, permission_name))
-        self.connection.commit()
+        self._cursor.execute("UPDATE permissions SET roles = ? WHERE permission_name = ?", (roles, permission_name))
+        self._connection.commit()
 
     def get_users(self, permission_name: str) -> list[int]:
         if self._check_module(permission_name, True):
             raise ValueError("Permission name must start with module id")
 
-        users = self.cursor.execute("SELECT users FROM permissions WHERE permission_name = ?", (permission_name,)).fetchone()
+        users = self._cursor.execute("SELECT users FROM permissions WHERE permission_name = ?", (permission_name,)).fetchone()
         if users is None:
             users = []
         else:
@@ -126,7 +134,7 @@ class PermissionsCog(ModuleCog):
         if self._check_module(permission_name, True):
             raise ValueError("Permission name must start with module id")
 
-        roles = self.cursor.execute("SELECT roles FROM permissions WHERE permission_name = ?", (permission_name,)).fetchone()
+        roles = self._cursor.execute("SELECT roles FROM permissions WHERE permission_name = ?", (permission_name,)).fetchone()
         if roles is None:
             roles = []
         else:
@@ -135,11 +143,11 @@ class PermissionsCog(ModuleCog):
 
     def contains_permission(self, permission_name: str, user: discord.Member) -> bool:
         if self._check_module(permission_name, True):
-            raise ValueError("Permission name must start with module id or \"*\"")#
+            raise ValueError("Permission name must start with module id or \"*\"")
 
         # Admin Permissions
 
-        users = self.cursor.execute("SELECT users FROM permissions WHERE permission_name = ?", ("*.admin",)).fetchone()
+        users = self._cursor.execute("SELECT users FROM permissions WHERE permission_name = ?", ("*.admin",)).fetchone()
 
         if users is None:
             return False
@@ -152,7 +160,7 @@ class PermissionsCog(ModuleCog):
 
         # General Permissions
 
-        users = self.cursor.execute("SELECT users FROM permissions WHERE permission_name = ?", (permission_name,)).fetchone()
+        users = self._cursor.execute("SELECT users FROM permissions WHERE permission_name = ?", (permission_name,)).fetchone()
 
         if users is None:
             return False
@@ -163,14 +171,14 @@ class PermissionsCog(ModuleCog):
         if str(user.id) in users:
             return True
 
-        roles = self.cursor.execute("SELECT roles FROM permissions WHERE permission_name = ?", (permission_name,)).fetchone()[0]
+        roles = self._cursor.execute("SELECT roles FROM permissions WHERE permission_name = ?", (permission_name,)).fetchone()[0]
         roles = roles.split(",") if roles else []
         for role in user.roles:
             if str(role.id) in roles:
                 return True
 
     def list_permissions(self) -> list[str]:
-        return [x[0] for x in self.cursor.execute("SELECT permission_name FROM permissions").fetchall()]
+        return [x[0] for x in self._cursor.execute("SELECT permission_name FROM permissions").fetchall()]
 
     def get_user_permissions(self, user: discord.Member) -> list[str]:
         permissions = []
